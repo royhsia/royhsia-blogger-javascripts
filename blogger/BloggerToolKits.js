284def28d36a9760b7bf6fd837c14d5122a5b9cd
royhsia@gmail.com <royhsia@gmail.com@f4bf1074-5b59-11de-9a1b-6103e4980ab8>
2014-09-03 03:39:14 +0000

    Add Blogger NCR(no country redirect) function

--- blogger/BloggerToolKits.js
+++ blogger/BloggerToolKits.js
@@ -119,3 +119,16 @@ function AutoClassNameToShortLink (TargetClassName, AutoClassNameToShortLinkSpan
   temp = temp + '</ul>';
   document.getElementById (AutoClassNameToShortLinkSpan).innerHTML = temp;
 }
+
+//
+// Blogger NCR (no country redirect)
+function BloggerNoCountryRedirect () {
+  var blog = document.location.hostname;
+  var slug = document.location.pathname;
+  var ctld = blog.substr(blog.lastIndexOf("."));
+  if (ctld != ".com") {
+    var ncr = "http://" + blog.substr(0, blog.indexOf("."));
+    ncr += ".blogspot.com/ncr" + slug;
+    window.location.replace(ncr);
+  }
+}
