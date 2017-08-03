4b68c99df551c2c3a43b3c166fb9af6c28131cb4
royhsia <royhsia@gmail.com>
2017-08-03 22:36:05 +0800

    New function XPCed2kParser as "XPCed2kMain" 2.0
    Example 1 (Blogger/Blogspot by Google):
    <script type="text/javascript">XPCed2kParser (`ed2k://|file|...|/<br />
    ed2k://|file|...|/`);</script>
    
    Example 2:
    <script type="text/javascript">XPCed2kParser (`ed2k://|file|...|/
    ed2k://|file|...|/`);</script>

--- blogger/ed2kscriptByXPC.js
+++ blogger/ed2kscriptByXPC.js
@@ -1,19 +1,19 @@
-// 
-//  ed2kscriptByXBJ.js
-//  Downloads
-//  
-//  Created by Roranicus on 2013-08-26. v1.0
-//   Modify by Roranicus on 2014-07-27. v1.1 fix file name with %
-//   Modify by Roranicus on 2014-07-30. v1.2 fix autocomplete
-//   Modify by Roranicus on 2014-08-03. v1.3 remove time delay workaround, add Math.random();
-//  Copyright 2013 Roranicus. All rights reserved.
-// 
 /*
+ 
+  ed2kscriptByXBJ.js
+  Downloads
+  
+  Created by Roranicus on 2013-08-26. v1.0
+   Modify by Roranicus on 2014-07-27. v1.1 fix file name with %
+   Modify by Roranicus on 2014-07-30. v1.2 fix autocomplete
+   Modify by Roranicus on 2014-08-03. v1.3 remove time delay workaround, add Math.random();
+   Modify by Roranicus on 2017-08-03. v2.0 use `` (&grave;) to accomplish use plain text as input method
+
   TODO Bug list: 1. ed2k link error function will not work
                  2. (fix) checkbox won't auto-re-check after uncheck and reload the page
 */
 
-function XPCed2k_CalculateSize (size) {
+function CalculateSize_ed2kXPC (size) {
   //
   // you can use either "navigator.platform" or "navigator.userAgent", this is for 
   // Mac calculate data size using 1000 bytes as 1KB and windows use 1024 bytes as 1KB.
@@ -39,7 +39,7 @@ function XPCed2k_CalculateSize (size) {
   return size + unit;
 }
 
-function XPCed2k_UpdateTotalSize (ed2kScriptName) {
+function UpdateTotalSize_ed2kXPC (ed2kScriptName) {
   var ed2k_list = document.getElementsByName(ed2kScriptName);
   var IsCheckboxAllChecked = document.getElementById("SelectAll"+ed2kScriptName);
   var temp, TotalSize = 0;
@@ -54,19 +54,19 @@ function XPCed2k_UpdateTotalSize (ed2kScriptName) {
     }
   }
 
-  document.getElementById("SizeOf" + ed2kScriptName).innerHTML = XPCed2k_CalculateSize(TotalSize);;
+  document.getElementById("SizeOf" + ed2kScriptName).innerHTML = CalculateSize_ed2kXPC(TotalSize);;
 }
 
-function XPCed2k_DoSelectAll (ed2kScriptName, checked) {
+function DoSelectAll_ed2kXPC (ed2kScriptName, checked) {
   var ed2k_list = document.getElementsByName(ed2kScriptName);
 
   for (var i = 0; i < ed2k_list.length; i++) {
     ed2k_list[i].checked = checked;
   }
-  XPCed2k_UpdateTotalSize(ed2kScriptName);
+  UpdateTotalSize_ed2kXPC(ed2kScriptName);
 }  
 
-function XPCed2k_DownloadSelectItems(ed2kScriptName, CurrentIndex, IsFirst) {
+function DownloadSelectItems_ed2kXPC (ed2kScriptName, CurrentIndex, IsFirst) {
   var ed2k_list = document.getElementsByName(ed2kScriptName);
   var timeout;
 
@@ -75,46 +75,98 @@ function XPCed2k_DownloadSelectItems(ed2kScriptName, CurrentIndex, IsFirst) {
       window.location = ed2k_list[i].value;
       timeout = IsFirst ? 6000 : 500;
       i++;
-      window.setTimeout("XPCed2k_DownloadSelectItems('" + ed2kScriptName + "'," + i + ",0);", timeout);
+      window.setTimeout("DownloadSelectItems_ed2kXPC('" + ed2kScriptName + "'," + i + ",0);", timeout);
       break;
     }
   }
+}
+
+function UniqueName_ed2kXPC () {
+  //
+  // declear a unique id name from current time for every ed2k link groups
+  var Time = new Date();
+  var random = Math.random();
+
+  return 'ed2kScript' + Time.getSeconds().toString() + Time.getMilliseconds().toString() + random;
+}
+
+function FileNameCheck_ed2kXPC (fileName) {
+  var temp = fileName;
+  var checkForPercentage = fileName.match ('%'); // check for if % exist, if exist will do 'File name check #1'
+  var splitDecodablePercentage = fileName.split(/%[0-9a-fA-F]{2}/g);    // check for decodable (decodeURIComponent), save splited strings
+
+  //
+  // File name check #1: check for alone %, decodable URI component and remove it
+  if (splitDecodablePercentage != fileName) { // if these two are the same means temp[2] doesn't have %xx
+    var stringToGlue = "";
+    var matchDecodablePercentage = fileName.match(/%[0-9a-fA-F]{2}/g); // check for decodable (decodeURIComponent), save decodable strings
+    for (var glueIndex = 0; glueIndex < splitDecodablePercentage.length; glueIndex++ ) {
+      splitDecodablePercentage[glueIndex] = splitDecodablePercentage[glueIndex].replace('%',''); // remove single % in splitDecodablePercentage[i]
+      
+      //
+      // glue strings back together
+      if (glueIndex < matchDecodablePercentage.length) {
+        stringToGlue = stringToGlue + splitDecodablePercentage[glueIndex] + matchDecodablePercentage[glueIndex];
+      } else {
+        stringToGlue = stringToGlue + splitDecodablePercentage[glueIndex]; // for the last string
+      }
+    }
+    temp = stringToGlue;
+  } else if (checkForPercentage) { // File name check #1, else part: means temp[2] have single/seperate % exist
+    var removeSinglePercentage = temp[2].split(/%/g);
+    var stringToGlue = "";
+    for (var glueIndex = 0; glueIndex < removeSinglePercentage.length; glueIndex++ ) {
+      stringToGlue = stringToGlue + removeSinglePercentage[glueIndex];
+    }
+    temp = stringToGlue;
+  }
+
+  return temp;
 }
 
 //
 // This function isn't missing arguments/parameters, I use "arguments" to get arguments.
 // NOTE: if ed2k_link has symbol like ' (single quotation mark) or " (quotation mark), it will make big mistake...... 
 //       <s>arguments doesn't support ed2k name start with '[' or ']'. I feel so Orz</s>
-function XPCed2kMain () {  
-  //
-  // declear a unique id name from current time for every ed2k link groups
-  var Time = new Date();
-  var random = Math.random();
+function XPCed2kMain () {
+  var arg;
   var Total_ed2k_Size = 0;
-  var ed2kScriptName = 'ed2kScript' + Time.getSeconds().toString() + Time.getMilliseconds().toString() + random;
+  var ed2kScriptName = UniqueName_ed2kXPC();
+
+  //
+  // determine if it's original XPCed2kMain or XPCed2kParser
+  if (typeof arguments[0] === 'object') {
+    arg = arguments[0]; // XPCed2kParser
+  } else if (typeof arguments[0] === 'string') {
+    arg = arguments;    // XPCed2kMain (original)
+  } else {
+    // there must be something wrong
+    console.log (">>> fatal error: arguments not valid");
+    return 0;
+  }
 
   document.write('<table class="ed2kXPCtableStyle">');
-  for (var i = 0; i < arguments.length; i++) {
+  for (var i = 0; i < arg.length; i++) {
     //
     // split arguments (emule_link)
     // temp [0] contains ed2k declare "ed2k://"
     // temp [1] contains ed2k declare "file" 
     // temp [2] contains "file name"
     // temp [3] contains "file size"
-    // temp [4] contains "file hash value", others ignore.
-    var temp = arguments[i].split('|');
+    // temp [4] contains "file hash value"
+    // temp [5], [6] ...  ignore.
+    var temp = arg[i].split('|');
 
     //
     // basic ed2k_link error check
     if ((temp[0] != 'ed2k://') || (temp[1] != 'file') || (temp[4].length != 32)) {
       //
       // checksum error
-      document.write('<tr><td></td><td class="XPCed2kFileNameArea">ed2k link error</td><td class="XPCed2kFileSize"></td></tr>');
+      document.write('<tr><td colspan="3" class="XPCed2kFileNameArea">ed2k link error</td></tr>');
       break;
     } 
 
     /*
-        //
         // backup: easy method for file name replace
         if (temp [2].replace(/%[0-9a-fA-F]{2}/g,"").match(/%/g)){
           var fileType = temp [2].replace(/%[0-9a-fA-F]{2}/g,"").split(".");
@@ -122,47 +174,20 @@ function XPCed2kMain () {
         }
     */
 
-    var checkForPercentage = temp[2].match ('%'); // check for if % exist, if exist will do 'File name check #1'
-    var splitDecodablePercentage = temp[2].split(/%[0-9a-fA-F]{2}/g);    // check for decodable (decodeURIComponent), save splited strings
-
-    //
-    // File name check #1: check for alone %, decodable URI component and remove it
-    if (splitDecodablePercentage != temp[2]) { // if these two are the same means temp[2] doesn't have %xx
-      var stringToGlue = "";
-      var matchDecodablePercentage = temp [2].match(/%[0-9a-fA-F]{2}/g); // check for decodable (decodeURIComponent), save decodable strings
-      for (var glueIndex = 0; glueIndex < splitDecodablePercentage.length; glueIndex++ ) {
-        splitDecodablePercentage[glueIndex] = splitDecodablePercentage[glueIndex].replace('%',''); // remove single % in splitDecodablePercentage[i]
-
-        //
-        // glue strings back together
-        if (glueIndex < matchDecodablePercentage.length) {
-          stringToGlue = stringToGlue + splitDecodablePercentage[glueIndex] + matchDecodablePercentage[glueIndex];
-        } else {
-          stringToGlue = stringToGlue + splitDecodablePercentage[glueIndex]; // for the last string
-        }
-      }
-      temp [2] = stringToGlue;
-    } else if (checkForPercentage) { // File name check #1, else part: means temp[2] have single/seperate % exist
-      var removeSinglePercentage = temp[2].split(/%/g);
-      var stringToGlue = "";
-      for (var glueIndex = 0; glueIndex < removeSinglePercentage.length; glueIndex++ ) {
-        stringToGlue = stringToGlue + removeSinglePercentage[glueIndex];
-      }
-      temp [2] = stringToGlue;
-    }
+    temp[2] = FileNameCheck_ed2kXPC (temp[2]);
 
     document.write('<tr>');
     //
     // print checkbox and HTML code
-    document.write('<td><input type="checkbox" checked="checked" name="' + ed2kScriptName + '" value="' + arguments[i] + '" onclick="XPCed2k_UpdateTotalSize(\'' + ed2kScriptName + '\');" autocomplete="off" /></td><td class="XPCed2kFileNameArea"><a href="' + arguments[i] + '">' + unescape(decodeURIComponent(temp[2])) + '</a></td>');
+    document.write('<td><input type="checkbox" checked="checked" name="' + ed2kScriptName + '" value="' + arg[i] + '" onclick="UpdateTotalSize_ed2kXPC(\'' + ed2kScriptName + '\');" autocomplete="off" /></td><td class="XPCed2kFileNameArea"><a href="' + arg[i] + '">' + unescape(decodeURIComponent(temp[2])) + '</a></td>');
     //
     // print ed2k file size
     Total_ed2k_Size += parseInt(temp[3]);
-    document.write('<td class="XPCed2kFileSize">' + XPCed2k_CalculateSize(temp[3]) + '</td>');
+    document.write('<td class="XPCed2kFileSize">' + CalculateSize_ed2kXPC(temp[3]) + '</td>');
     document.write('</tr>');
   }
 
-  document.write('<tr><td><input type="checkbox" checked="checked" id="SelectAll' + ed2kScriptName + '" onClick="XPCed2k_DoSelectAll(\'' + ed2kScriptName + '\',this.checked)" autocomplete="off" /></td><td class="XPCed2kFileNameArea"><label for="SelectAll' + ed2kScriptName +'">select all</label>&nbsp;&nbsp;&nbsp;&nbsp;<input value="Download selected item(s)" onclick="XPCed2k_DownloadSelectItems(\'' + ed2kScriptName + '\',0,1)" type="button"></td><td class="XPCed2kFileSize" style="color:red; font-weight:bold;" id="SizeOf' + ed2kScriptName +'">' + XPCed2k_CalculateSize(Total_ed2k_Size) +'</td></tr>');
+  document.write('<tr><td><input type="checkbox" checked="checked" id="SelectAll' + ed2kScriptName + '" onClick="DoSelectAll_ed2kXPC(\'' + ed2kScriptName + '\',this.checked)" autocomplete="off" /></td><td class="XPCed2kFileNameArea"><label for="SelectAll' + ed2kScriptName +'">select all</label>&nbsp;&nbsp;&nbsp;&nbsp;<input value="Download selected item(s)" onclick="DownloadSelectItems_ed2kXPC(\'' + ed2kScriptName + '\',0,1)" type="button"></td><td class="XPCed2kFileSize" style="color:red; font-weight:bold;" id="SizeOf' + ed2kScriptName +'">' + CalculateSize_ed2kXPC(Total_ed2k_Size) +'</td></tr>');
   document.write('</table>');
 
   //
@@ -170,3 +195,22 @@ function XPCed2kMain () {
   // I don't know why '.XPCed2kFileNameArea' and '.XPCed2kFileSize' has to be right after '#ed2kXPCtableStyle', otherwise it won't work.
   document.write('<style type="text/css">.ed2kXPCtableStyle { width:99%; margin:auto; font-size:15px; line-height:initial; max-width:600px; min-width:400px;} .ed2kXPCtableStyle td {background-color: rgb(243, 247, 253);} .XPCed2kFileNameArea {word-break:break-all; padding:0.2em;} .XPCed2kFileSize {width: 5em; padding-right: 7px; text-align:right; font-family:Courier;}</style>');
 }
+
+//
+// Version 2.0 of XPCed2kMain
+// This function uses `` to parse whatever it is between two grave accents (including line-break [\r\n]).
+//  Note: There can NOT have any "`" in ed2k filename.
+//
+//  See "Template literals" in MDN under section "Multi-line strings" for more detail
+function XPCed2kParser () {
+  if (arguments[0].indexOf ("<br />\n") > -1) {
+    var pass = arguments[0].split("<br />\n");
+  } else {
+    var pass = arguments[0].split("\n");
+  }
+  XPCed2kMain (pass);
+}
+
+/*
+  Copyright 2013-2017 Roranicus. All rights reserved.
+*/
